8f6faf17370f8145900e26d3941d9e0f19909b38
Fiacre Sankara <fiacre2000@gmail.com>
2018-04-11 18:08:04 +0000

    Configure starting point + clean up

--- ext_tables.php
+++ ext_tables.php
@@ -13,14 +13,12 @@ call_user_func(
 
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('accounting_system', 'Configuration/TypoScript', 'Accounting System');
 
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_accountingsystem_domain_model_patient', 'EXT:accounting_system/Resources/Private/Language/locallang_csh_tx_accountingsystem_domain_model_patient.xlf');
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_accountingsystem_domain_model_patient');
 
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_accountingsystem_domain_model_consultation', 'EXT:accounting_system/Resources/Private/Language/locallang_csh_tx_accountingsystem_domain_model_consultation.xlf');
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_accountingsystem_domain_model_consultation');
 
-        \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_accountingsystem_domain_model_facture', 'EXT:accounting_system/Resources/Private/Language/locallang_csh_tx_accountingsystem_domain_model_facture.xlf');
         \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_accountingsystem_domain_model_facture');
 
     }
 );
+$GLOBALS['TCA']['tt_content']['types']['list']['subtypes_excludelist']['accountingsystem_accountingsystem'] = 'select_key,recursive,pages';
