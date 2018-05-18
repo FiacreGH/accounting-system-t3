7f5a4d1277d4f48b4b69966d0427d46cedf8cd59
Fiacre Sankara <fiacre2000@gmail.com>
2018-05-18 09:50:58 +0000

    Best implementation for the deletion confirmation

--- Resources/Public/JavaScript/Additionnal/additionnal.js
+++ Resources/Public/JavaScript/Additionnal/additionnal.js
@@ -1,11 +1,24 @@
 /**
  * Created by fsankara.
  */
-$(document).ready( function () {
+
+$(document).ready( function (event) {
+
+	//datatables implementation
 	$('#table_id').DataTable({
 		//loading french langage
 		"language": {
 			"url": "//cdn.datatables.net/plug-ins/1.10.16/i18n/French.json"
 		}
 	});
-} );
+
+	//deletion confirmation
+	$('.delete-confirm').click(function(){
+		if (!confirm('Êtes-vous suûr de vouloir supprimer ?')){
+		}
+		else{
+			event.preventDefault();
+		}
+		return false;
+	});
+});
